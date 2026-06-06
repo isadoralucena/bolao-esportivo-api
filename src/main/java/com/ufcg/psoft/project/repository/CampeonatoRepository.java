0fb0758155d56f2f2b846966df2c27f377f58b56
adson <adsongabrielfsilva@gmail.com>
2026-06-06 13:35:09 -0300

    feat(us1): create campeonato repository

---
+++ src/main/java/com/ufcg/psoft/project/repository/CampeonatoRepository.java
@@ -0,0 +1,14 @@
+package com.ufcg.psoft.project.repository;
+
+import com.ufcg.psoft.project.model.Campeonato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CampeonatoRepository extends JpaRepository<Campeonato, Long> {
+    List<Campeonato> findByNomeContaining(String nome);
+    Optional<Campeonato> findByCodigo(String codigo);
+}
