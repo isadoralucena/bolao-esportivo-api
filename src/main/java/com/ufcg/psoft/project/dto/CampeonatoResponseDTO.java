247478d55a13cde7fee30c8063cbb2ff93490ffe
adson <adsongabrielfsilva@gmail.com>
2026-06-06 15:38:22 -0300

    feat(us1): add dtos for campeonato

---
+++ src/main/java/com/ufcg/psoft/project/dto/CampeonatoResponseDTO.java
@@ -0,0 +1,40 @@
+package com.ufcg.psoft.project.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ufcg.psoft.project.model.Campeonato;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CampeonatoResponseDTO {
+	private Long id;
+
+	@JsonProperty("nome")
+	private String nome;
+
+	@JsonProperty("url")
+	private String url;
+
+	@JsonProperty("codigo")
+	private String codigo;
+
+	@JsonProperty("ativo")
+	private Boolean ativo;
+
+	public CampeonatoResponseDTO(Campeonato campeonato) {
+		this.id = campeonato.getId();
+		this.nome = campeonato.getNome();
+		this.url = campeonato.getUrl();
+		this.codigo = campeonato.getCodigo();
+		this.ativo = campeonato.getAtivo();
+	}
+}
