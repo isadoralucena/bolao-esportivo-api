685e645a9953d3d909dc09fca0b0315b0a3101c7
Isadora Lucena <isadorabeatriz595@gmail.com>
2026-06-07 19:26:26 -0300

    refactor(us1): standardize CampeonatoController routes to use /campeonatos

--- src/main/java/com/ufcg/psoft/project/controller/CampeonatoController.java
+++ src/main/java/com/ufcg/psoft/project/controller/CampeonatoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(
-value = "/campeonato",
+value = "/campeonatos",
 produces = MediaType.APPLICATION_JSON_VALUE
 )
 public class CampeonatoController {
