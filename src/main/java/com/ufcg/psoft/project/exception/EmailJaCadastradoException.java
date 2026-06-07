c4266edb4fcbd8cbe684d07c207840970a8cd5f8
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-06-07 01:16:04 -0300

    feat: new duplicate email exception

--- src/main/java/com/ufcg/psoft/project/exception/EmailJaCadastradoException.java
+++ src/main/java/com/ufcg/psoft/project/exception/EmailJaCadastradoException.java
@@ -2,6 +2,6 @@ package com.ufcg.psoft.project.exception;
 
 public class EmailJaCadastradoException extends ProjectException {
     public EmailJaCadastradoException() {
-        super("Já existe outro usuário com esse email cadastrado!");
+        super("Ja existe outro usuario com esse email cadastrado!");
     }
 }
