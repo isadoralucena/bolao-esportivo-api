b3e36d4cdfb3a0ca70ae28ed71b44bd20cd7d67d
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-06-07 01:15:46 -0300

    fix: normalizing email and fixing duplicate email check

--- src/main/java/com/ufcg/psoft/project/service/usuario/UsuarioServiceImpl.java
+++ src/main/java/com/ufcg/psoft/project/service/usuario/UsuarioServiceImpl.java
@@ -28,19 +28,28 @@ public class UsuarioServiceImpl implements UsuarioService {
         if (!usuario.getCodigo().equals(codigoAcesso)) {
             throw new CodigoDeAcessoInvalidoException();
         }
+
+        String novoEmailNormalizado = usuarioPostPutRequestDTO.getEmail().trim().toLowerCase();
+        String emailAtualNormalizado = usuario.getEmail().trim().toLowerCase();
+        if (!emailAtualNormalizado.equals(novoEmailNormalizado) && usuarioRepository.existsByEmail(novoEmailNormalizado))  {
+            throw new EmailJaCadastradoException();
+        }
+
         modelMapper.map(usuarioPostPutRequestDTO, usuario);
+        usuario.setEmail(emailAtualNormalizado);
         usuarioRepository.save(usuario);
         return modelMapper.map(usuario, UsuarioResponseDTO.class);
     }
 
     @Override
     public UsuarioResponseDTO criar(UsuarioPostPutRequestDTO usuarioPostPutRequestDTO) {
-        String email = usuarioPostPutRequestDTO.getEmail().trim().toLowerCase();
-        if (usuarioRepository.existsByEmail(email)) {
+        String emailNormalizado = usuarioPostPutRequestDTO.getEmail().trim().toLowerCase();
+        if (usuarioRepository.existsByEmail(emailNormalizado)) {
             throw new EmailJaCadastradoException();
         }
 
         Usuario usuario = modelMapper.map(usuarioPostPutRequestDTO, Usuario.class);
+        usuario.setEmail(emailNormalizado);
         usuarioRepository.save(usuario);
         return modelMapper.map(usuario, UsuarioResponseDTO.class);
     }
