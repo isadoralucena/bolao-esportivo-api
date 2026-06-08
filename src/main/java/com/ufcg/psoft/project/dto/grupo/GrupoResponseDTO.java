caed5c98263ef2ec2e8a9996a1d2f974ed1d601a
Isadora Lucena <isadorabeatriz595@gmail.com>
2026-06-08 17:34:58 -0300

    feat(us3): create request and response DTOs

--- src/main/java/com/ufcg/psoft/project/dto/grupo/GrupoResponseDTO.java
+++ src/main/java/com/ufcg/psoft/project/dto/grupo/GrupoResponseDTO.java
@@ -1,5 +1,8 @@
 package com.ufcg.psoft.project.dto.grupo;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ufcg.psoft.project.dto.usuario.UsuarioResponseDTO;
 import com.ufcg.psoft.project.dto.campeonato.CampeonatoResponseDTO;
@@ -15,6 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GrupoResponseDTO {
+    @JsonProperty("id")
     private Long id;
 
     @JsonProperty("nome")
@@ -29,11 +33,17 @@ public class GrupoResponseDTO {
     @JsonProperty("organizador")
     private UsuarioResponseDTO organizador;
 
+    @JsonProperty("participantes")
+    private Set<UsuarioResponseDTO> participantes;
+
     public GrupoResponseDTO(Grupo grupo) {
         this.id = grupo.getId();
         this.nome = grupo.getNome();
         this.descricao = grupo.getDescricao();
         this.campeonato = new CampeonatoResponseDTO(grupo.getCampeonato());
         this.organizador = new UsuarioResponseDTO(grupo.getOrganizador());
+        this.participantes = grupo.getParticipantes().stream()
+                .map(UsuarioResponseDTO::new)
+                .collect(Collectors.toSet());
     }
 }
