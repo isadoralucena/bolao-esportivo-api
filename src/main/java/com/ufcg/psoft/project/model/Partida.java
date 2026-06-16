502712eabc20b737e71e924aac10f36e601a3cbd
adson <adsongabrielfsilva@gmail.com>
2026-06-16 12:33:41 -0300

    feat: create partida

---
+++ src/main/java/com/ufcg/psoft/project/model/Partida.java
@@ -0,0 +1,56 @@
+package com.ufcg.psoft.project.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Partida {
+
+    @JsonProperty("id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "campeonato_id", nullable = false)
+    private Campeonato campeonato;
+
+    @JsonProperty("codigoExterno")
+    @Column(nullable = false)
+    private Long codigoExterno;
+
+    @JsonProperty("mandante")
+    @Column(nullable = false)
+    private String mandante;
+
+    @JsonProperty("visitante")
+    @Column(nullable = false)
+    private String visitante;
+
+    @JsonProperty("golsMandante")
+    private Integer golsMandante;
+
+    @JsonProperty("golsVisitante")
+    private Integer golsVisitante;
+
+    @JsonProperty("data")
+    @Column(nullable = false)
+    private LocalDateTime data;
+
+    @JsonProperty("status")
+    @Column(nullable = false)
+    private String status;
+
+    @JsonProperty("rodada")
+    private Integer rodada;
+}
