a8386f03ed1146fd09e25c3c20f1160c6620dcad
ErikAlvesAlmeida <eriikalves55@gmail.com>
2026-06-21 21:14:41 -0300

    feat: add metodo editar na interface PalpiteService

--- src/main/java/com/ufcg/psoft/project/service/palpite/PalpiteService.java
+++ src/main/java/com/ufcg/psoft/project/service/palpite/PalpiteService.java
@@ -14,4 +14,6 @@ public interface PalpiteService {
     List<PalpiteResponseDTO> listarPorGrupoEPartida(Long grupoId, Long partidaId);
 
     List<PalpiteResponseDTO> listarPorUsuario(Long usuarioId);
+
+    PalpiteResponseDTO editar(Long palpiteId, Long usuarioId, String codigo, PalpitePostPutRequestDTO dto);
 }
