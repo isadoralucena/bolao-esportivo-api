4641e2f193f3dbb825dfd40a84c86feb2aaec2e0
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-06-23 17:26:55 -0300

    refactor: removing sync endpoint from classificacao and standardazing

--- src/main/java/com/ufcg/psoft/project/controller/ClassificacaoCampeonatoController.java
+++ src/main/java/com/ufcg/psoft/project/controller/ClassificacaoCampeonatoController.java
@@ -1,31 +1,26 @@
 package com.ufcg.psoft.project.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ufcg.psoft.project.dto.campeonato.ClassificacaoCampeonatoResponseDTO;
 import com.ufcg.psoft.project.service.campeonato.ClassificacaoCampeonatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/campeonatos")
+@RequestMapping(
+    value = "/campeonatos",
+    produces = MediaType.APPLICATION_JSON_VALUE
+)
 public class ClassificacaoCampeonatoController {
 
     @Autowired
     private ClassificacaoCampeonatoService classificacaoCampeonatoService;
 
-    @GetMapping("/{campeonatoId}/classificacao")
-    public List<ClassificacaoCampeonatoResponseDTO> listarClassificacao(@PathVariable Long campeonatoId) {
-        return classificacaoCampeonatoService.listarPorCampeonato(campeonatoId);
-    }
-
-    @PostMapping("/{campeonatoId}/classificacao/sincronizar")
-    public void sincronizarClassificacao(@PathVariable Long campeonatoId) {
-        classificacaoCampeonatoService.sincronizarClassificacao(campeonatoId);
+    @GetMapping("{campeonatoId}/classificacao")
+    public ResponseEntity<?> listarClassificacao(@PathVariable Long campeonatoId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(classificacaoCampeonatoService.listarPorCampeonato(campeonatoId));
     }
 }
