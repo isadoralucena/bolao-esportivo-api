0fbb14860aa7249637661279aba71b3fe0abb456
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-06-23 14:04:42 -0300

    feat: new sync scheduler and variables

---
+++ src/main/java/com/ufcg/psoft/project/service/partida/SincronizacaoPartidas.java
@@ -0,0 +1,40 @@
+package com.ufcg.psoft.project.service.partida;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.ufcg.psoft.project.model.Campeonato;
+import com.ufcg.psoft.project.repository.CampeonatoRepository;
+
+@Service
+public class SincronizacaoPartidas {
+
+    @Autowired
+    private CampeonatoRepository campeonatoRepository;
+
+    @Autowired
+    private PartidaService partidaService;
+
+    @Value("${project.sync.max-requisicoes-por-ciclo}")
+    private int maxRequisicoesPorCiclo;
+
+    @Scheduled(fixedDelayString = "${project.sync.scheduler-delay-ms}")
+    public void sincronizarPartidas() {
+        List<Campeonato> campeonatos = campeonatoRepository.findByAtivoTrueOrderByUltimaSincronizacaoAsc();
+
+        int requisicoesFeitas = 0;
+
+        for (Campeonato campeonato : campeonatos) {
+            if (requisicoesFeitas >= maxRequisicoesPorCiclo) {
+                break;
+            }
+
+            partidaService.sincronizarPartidas(campeonato);
+            requisicoesFeitas++;
+        }
+    }
+}
