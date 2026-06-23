e01bc2a00abc519a75a5af971fa06a9837ea346b
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-06-23 15:48:23 -0300

    feat: adding classificacao

---
+++ src/main/java/com/ufcg/psoft/project/service/campeonato/ClassificacaoCampeonatoService.java
@@ -0,0 +1,11 @@
+package com.ufcg.psoft.project.service.campeonato;
+
+import java.util.List;
+
+import com.ufcg.psoft.project.dto.campeonato.ClassificacaoCampeonatoResponseDTO;
+import com.ufcg.psoft.project.model.Campeonato;
+
+public interface ClassificacaoCampeonatoService {
+    void sincronizarClassificacao(Campeonato campeonato);
+    List<ClassificacaoCampeonatoResponseDTO> listarPorCampeonato(Long campeonatoId);
+}
