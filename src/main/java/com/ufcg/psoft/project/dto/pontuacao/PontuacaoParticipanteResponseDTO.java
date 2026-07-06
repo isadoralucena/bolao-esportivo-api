dd5fcbe5d935f20a4be9bf207f39ca3a9412e3b4
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-07-05 23:54:56 -0300

    feat: new dto for participante points

--- src/main/java/com/ufcg/psoft/project/dto/pontuacao/PontuacaoParticipanteResponseDTO.java
+++ src/main/java/com/ufcg/psoft/project/dto/pontuacao/PontuacaoParticipanteResponseDTO.java
@@ -1,6 +1,7 @@
 package com.ufcg.psoft.project.dto.pontuacao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ufcg.psoft.project.model.Usuario;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,35 @@ public class PontuacaoParticipanteResponseDTO {
     @JsonProperty("usuarioId")
     private Long usuarioId;
 
-    @JsonProperty("pontos")
-    private Integer pontos;
+    @JsonProperty("usuarioNome")
+    private String usuarioNome;
+
+    @JsonProperty("pontuacao")
+    private Integer pontuacao;
+
+    @JsonProperty("acertosVencedor")
+    private Integer acertosVencedor;
+
+    @JsonProperty("acertosEmpate")
+    private Integer acertosEmpate;
+
+    @JsonProperty("placaresExatos")
+    private Integer placaresExatos;
+
+    public PontuacaoParticipanteResponseDTO(
+            Long grupoId,
+            Usuario usuario,
+            Integer pontuacao,
+            Integer acertosVencedor,
+            Integer acertosEmpate,
+            Integer placaresExatos
+    ) {
+        this.grupoId = grupoId;
+        this.usuarioId = usuario.getId();
+        this.usuarioNome = usuario.getNome();
+        this.pontuacao = pontuacao;
+        this.acertosVencedor = acertosVencedor;
+        this.acertosEmpate = acertosEmpate;
+        this.placaresExatos = placaresExatos;
+    }
 }
