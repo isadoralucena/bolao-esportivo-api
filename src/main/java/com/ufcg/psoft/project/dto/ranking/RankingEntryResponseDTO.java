0c663cd0581bcf8127caacf86acccddd83627d71
Yan Vitor de Lira Silva <yan113348393@gmail.com>
2026-07-08 19:03:45 -0300

    feat: add methods in punctuation

---
+++ src/main/java/com/ufcg/psoft/project/dto/ranking/RankingEntryResponseDTO.java
@@ -0,0 +1,7 @@
+package com.ufcg.psoft.project.dto.ranking;
+
+@Data
+
+public class RankingEntryResponseDTO {
+    
+}
---
+++ src/main/java/com/ufcg/psoft/project/service/ranking/RankingService.java
@@ -0,0 +1,5 @@
+package com.ufcg.psoft.project.service.ranking;
+
+public class RankingService {
+    
+}
