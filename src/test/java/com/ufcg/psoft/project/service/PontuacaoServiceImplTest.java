fc3cfbe15eef490c23105bee49aac6825e398e8b
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-07-23 17:58:22 -0300

    refactor: removing unused imports

--- src/test/java/com/ufcg/psoft/project/service/PontuacaoServiceImplTest.java
+++ src/test/java/com/ufcg/psoft/project/service/PontuacaoServiceImplTest.java
@@ -3,7 +3,6 @@ package com.ufcg.psoft.project.service;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
