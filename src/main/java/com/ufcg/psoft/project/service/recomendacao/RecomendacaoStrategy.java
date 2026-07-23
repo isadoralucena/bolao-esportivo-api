7e568c72e51013ce55b87c261db01a256a2d4bf6
ErikAlvesAlmeida <eriikalves55@gmail.com>
2026-07-23 20:03:19 -0300

    revert: remove commits da US20 enviados por engano na feat/entrega-04

--- src/main/java/com/ufcg/psoft/project/service/recomendacao/RecomendacaoStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.ufcg.psoft.project.service.recomendacao;
-
-import com.ufcg.psoft.project.dto.recomendacao.RecomendacaoResponseDTO;
-import com.ufcg.psoft.project.model.Partida;
-
-public interface RecomendacaoStrategy {
-    String getNome();
-    RecomendacaoResponseDTO recomendar(Partida partida);
-}
