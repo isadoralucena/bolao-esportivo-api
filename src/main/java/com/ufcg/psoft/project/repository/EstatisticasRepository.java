3a2d574acd3973da0803e6201640a55d91f21711
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-07-23 18:11:19 -0300

    feat: new repository methods

--- src/main/java/com/ufcg/psoft/project/repository/EstatisticasRepository.java
+++ src/main/java/com/ufcg/psoft/project/repository/EstatisticasRepository.java
@@ -1,8 +1,11 @@
 package com.ufcg.psoft.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ufcg.psoft.project.model.Estatisticas;
 
 public interface EstatisticasRepository extends JpaRepository<Estatisticas, Long> {
-    
+    Estatisticas findFirstByUsuarioIdOrderByDataRegistroDesc(Long usuarioId);
+    List<Estatisticas> findByUsuarioId(Long usuarioId);
 }
